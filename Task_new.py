bb551644d33cdf04773e1dc76951271875fd5610
Ilya Raspitin <i.raspitin@yandex.ru>
2020-10-27 22:52:09 +0300

    Fixed

--- Task_new.py
+++ Task_new.py
@@ -1,47 +1,39 @@
 import os, json, xml.etree.ElementTree as ET
 
-def get_words(file):
+def get_words(file, letters_number):
+    not_sorted_list = []
+
     if os.path.splitext(file)[1] == ".json":
-        with open("newsafr.json", encoding="utf-8") as f:
+        with open(file, encoding="utf-8") as f:
             json_data = json.load(f)
             items = json_data["rss"]["channel"]["items"]
-            b = []
             for x in items:
-                b.extend(x["description"].split(" "))
-        return [words for words in b
-                if len(words) >= 6]
+                not_sorted_list.extend(x["description"].split(" "))
 
     if os.path.splitext(file)[1] == ".xml":
         parser = ET.XMLParser(encoding="utf-8")
         tree = ET.parse("newsafr.xml", parser)
         root = tree.getroot()
         items = root.findall("channel/item")
-        b = []
         for x in items:
-            b.extend(x.find("description").text.split(" "))
-        return [words for words in b
-                if len(words) >= 6]
+            not_sorted_list.extend(x.find("description").text.split(" "))
 
-def get_sorted_(b):
-    forr = {}
-    for words in b:
-        if words in forr.keys():
-            forr[words] = forr[words] + 1
+    return [words for words in not_sorted_list if len(words) >= letters_number]
+
+def get_sorted_(not_sorted_list):
+    count_words = {}
+    for word in not_sorted_list:
+        if word in count_words.keys():
+            count_words[word] = count_words[word] + 1
         else:
-            forr[words] = 1
-    b = sorted(list(forr.items()), key=lambda x:-x[1])
-    return b
+            count_words[word] = 1
+    sorted_list = sorted(list(count_words.items()), key=lambda x:-x[1])
+    return sorted_list
 
-def print_ten(file):
-    x = 0
-    for words in get_sorted_(get_words(file)):
-        while x < 10:
-            print(get_sorted_(get_words(file))[x])
-            x = x + 1
-
+def print_some_words(file, words_number, letters_number):
+    print(get_sorted_(get_words(file, letters_number))[:words_number])
 
 def main():
     while True:
-        print_ten(input("Введите название файла "))
-
+        print_some_words(input("Введите название файла "), int(input("Введите количество слов для отображения ")), int(input("Введите минимальную длину слова ")))
 main()
